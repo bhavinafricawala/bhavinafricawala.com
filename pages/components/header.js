a2b6267fa8bc2b759fad4d995f45252794608b04
Africawala <bafricawala@alluma.org>
2019-10-04 14:25:37 -0700

    css changes

--- pages/components/header.js
+++ pages/components/header.js
@@ -3,12 +3,12 @@ import Head from 'next/head';
 
 const Header =()=>{
     return ( <Head>
-        <link rel="stylesheet" href="../stylesheets/App.css" />
-        <link rel="stylesheet" href="../stylesheets/About.css" />
-        <link rel="stylesheet" href="../stylesheets/bootstrap.min.css" />
-        <link rel="stylesheet" href="../stylesheets/Contact.css" />
-        <link rel="stylesheet" href="../stylesheets/Home.css" />
-        <link rel="stylesheet" href="../stylesheets/index.css" />
+        <link rel="stylesheet" href="/stylesheets/App.css" />
+        <link rel="stylesheet" href="/stylesheets/About.css" />
+        <link rel="stylesheet" href="/stylesheets/bootstrap.min.css" />
+        <link rel="stylesheet" href="/stylesheets/Contact.css" />
+        <link rel="stylesheet" href="/stylesheets/Home.css" />
+        <link rel="stylesheet" href="/stylesheets/index.css" />
     </Head>);
 }
 

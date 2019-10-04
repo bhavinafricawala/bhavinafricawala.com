10573cd1b920baa1a53ed1fec1f5c2e25d07df86
Africawala <bafricawala@alluma.org>
2019-10-04 11:10:31 -0700

    css path issue

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import HomePage from './components/homepage';
 import NavigationBar from './components/navbar';
 import BackGround from './components/background';
 import './stylesheets/App.css';
-import '/stylesheets/bootstrap.min.css';
-import '/stylesheets/Home.css';
+import './stylesheets/bootstrap.min.css';
+import './stylesheets/Home.css';
 
 
 const FirstPage = () => {

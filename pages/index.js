65362ba4939415677c2c23c0b4ca772edb2e97c1
Bhavin Africawala <bhavinafricawala@hotmail.com>
2019-10-03 22:59:25 -0700

    initial checkin

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const InputElement = () => {
+  return <input placeholder="Enter Some Text" />;
+};
+
+export default InputElement;

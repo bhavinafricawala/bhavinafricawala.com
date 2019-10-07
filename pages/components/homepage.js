f3d50591760a49c61a9bfb8a066e6f7e0538cd48
Africawala <bafricawala@alluma.org>
2019-10-07 15:37:01 -0700

    css background

--- pages/components/homepage.js
+++ pages/components/homepage.js
@@ -11,7 +11,30 @@ const HomePage=() => {
       <div>
         <h5>I make modern website with Front-End Technologies and C#</h5>
       </div>
-
+      <style jsx>{`
+      body{
+        background: #273552;
+      }
+      .homepost {
+        color: #ffffff;
+        display: grid;
+        max-width: 1000px;
+        margin: 150px auto;
+        grid-gap: 10px 50px;
+        grid-template-columns: 1fr 18fr 1fr;
+      }
+      
+      .homepost > * {
+        grid-column: 2 / -2;
+        text-align: left;
+      }
+      
+      .homepost p {
+        font-weight: bold;
+        font-size: 20px;
+      }
+      
+      `}</style>
     </div>
   );
 }

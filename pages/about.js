9bf14bb89c6ffd1eb962a3e58570860163e19f03
Africawala <bafricawala@alluma.org>
2019-10-08 15:40:34 -0700

    add about page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import NavigationBar from './components/navbar';
+import AboutPost from './components/aboutpost';
+import Resume from './components/resume';
 import Head from 'next/head';
 
-const About =()=>{
+const About = () => {
     
     return(
-    <div>
+    <div className="aboutpage">
         <Head>
-        <link rel="stylesheet" href="/static/bootstrap.min.css" />
-        <link rel="stylesheet" href="/static/App.css" />        
-        <link rel="stylesheet" href="/static/Home.css" />
-    </Head>
-    <NavigationBar pagename="about" />
+            <link rel="stylesheet" href="/static/bootstrap.min.css" />
+            <link rel="stylesheet" href="/static/App.css" />        
+            <link rel="stylesheet" href="/static/About.css" />
+        </Head>
+        <NavigationBar pagename="about" />
+        <div className="post">
+            <AboutPost />
+            <Resume />
+        </div>
     </div>);
 }
 

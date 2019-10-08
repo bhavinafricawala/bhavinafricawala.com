af31e392d5329ad38b9117efd17756368be4e593
Africawala <bafricawala@alluma.org>
2019-10-08 13:37:00 -0700

    about changes

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import NavigationBar from './components/navbar';
+import Head from 'next/head';
 
 const About =()=>{
-    return(<div>About</div>);
+    
+    return(
+    <div>
+        <Head>
+        <link rel="stylesheet" href="/static/bootstrap.min.css" />
+        <link rel="stylesheet" href="/static/App.css" />        
+        <link rel="stylesheet" href="/static/Home.css" />
+    </Head>
+    <NavigationBar pagename="about" />
+    </div>);
 }
 
 export default About;
